61e5d396b556fddb3e96af9e443db3f8f4bc3464
AB498 <abcd49800@gmail.com>
2024-02-09 21:32:06 +0600

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World");
 });
 app.get("/version", (req, res) => {
   res.send("v3");
