c3f5aa5ba43747ebd5401cb179dce72a745a5cdd
AB498 <abcd49800@gmail.com>
2024-02-10 20:31:39 +0600

    Sat, Feb 10, 2024  8:31:39 PM

--- server.js
+++ server.js
@@ -2,10 +2,42 @@ let express = require("express");
 
 let app = express();
 
-app.use("/", express.static("/static"));
+const directoryPath = "./static";
+
+app.use("/", express.static(directoryPath));
 
 app.get("/version", (req, res) => {
   res.send("v4");
+});
+
+app.get("/api/filesInfo", (req, res) => {
+  // Function to get last edited time for each file recursively
+  const result = {};
+  function getLastEditedTimes(dir, baseDir = "/") {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+
+    files.forEach((file) => {
+      const relativePath = path.join(baseDir, file.name); // Relative path calculation
+      const filePath = path.join(dir, file.name);
+      if (file.isDirectory()) {
+        getLastEditedTimes(filePath, relativePath);
+      } else {
+        const stats = fs.statSync(filePath);
+        const lastEditedTime = stats.mtime; // Last modified time
+        result[relativePath] = lastEditedTime;
+      }
+    });
+    for (const [key, value] of Object.entries(result)) {
+      const formattedPath = key.split(path.sep).join(path.posix.sep);
+      delete result[key];
+      result[formattedPath] = value;
+    }
+
+    return result;
+  }
+
+  const lastEditedTimes = getLastEditedTimes(directoryPath);
+  res.json(lastEditedTimes);
 });
 
 app.get("*", (req, res) => {
