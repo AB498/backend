6d718565ab0c3ba17892f0644087a6ad76ec7ce7
AB498 <abcd49800@gmail.com>
2024-02-10 21:44:21 +0600

    Sat, Feb 10, 2024  9:44:21 PM

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const directoryPath = "./static";
 app.use("/", express.static(directoryPath));
 
 app.get("/version", (req, res) => {
-  res.send("v4");
+  res.send("v5");
 });
 
 app.get("/api/filesInfo", (req, res) => {
