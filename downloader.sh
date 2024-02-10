29e8d18c5ec39f64f54faa84721bf529b581fed8
AB498 <abcd49800@gmail.com>
2024-02-10 21:34:08 +0600

    m

--- downloader.sh
+++ downloader.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
-# Function to perform git pull
 git_pull() {
     rm -rf .git/HEAD.lock .git/ORIG_HEAD* .git/refs/heads
     git fetch --all
-    git reset --hard origin/main
-    git pull origin main 
+    LOCAL=$(git rev-parse origin/main)
+    REMOTE=$(git rev-parse origin/main)
+    
+    if [ $LOCAL != $REMOTE ]; then
+        echo "Pulling changes..."
+        git reset --hard origin/main
+        git pull origin main
+    else
+        echo "Already up to date."
+    fi
 }
+
 
 git_pull
 
