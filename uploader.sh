e281c7e054fa2e563cfa8a2f8df008e1fc9ce5e5
AB498 <abcd49800@gmail.com>
2024-02-10 20:22:21 +0600

    Sat, Feb 10, 2024  8:22:21 PM

---
+++ uploader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to perform git push
+git_push() {
+    git add .
+    git commit -m "$(date)"
+    git push
+}
+
+# Run git pull initially
+git_push
+
+while true; do
+    sleep 5
+    git_push
+done
