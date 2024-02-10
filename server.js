2b54673db479adaeed7340d663bf44d623c68f1d
AB498 <abcd49800@gmail.com>
2024-02-10 20:25:53 +0600

    Sat, Feb 10, 2024  8:25:53 PM

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get("/version", (req, res) => {
   res.send("v4");
 });
 
+app.get("*", (req, res) => {
+  //spa
+  res.sendFile(__dirname + "/static/index.html");
+});
+
 // host 0.0.0.0
 let port = 8000;
 app.listen(port, "0.0.0.0", () => {
