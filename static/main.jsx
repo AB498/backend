69a805d65a0959d6c008feac1cac855c355943ea
AB498 <abcd49800@gmail.com>
2024-02-10 20:47:22 +0600

    Sat, Feb 10, 2024  8:47:22 PM

--- static/main.jsx
+++ static/main.jsx
@@ -686,7 +686,7 @@ let ProductTopNav = (props) => {
           Home
         </div>
         <div className="special-link" onClick={() => navigate("/discover")}>
-          Discover
+          Discover123
         </div>
         <div className="special-link" onClick={() => navigate("/hot")}>
           Hot
