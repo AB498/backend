b11be1883987c98cbf74d1620fb932cf9435ecca
AB498 <abcd49800@gmail.com>
2024-02-10 21:57:43 +0600

    Sat, Feb 10, 2024  9:57:43 PM

--- downloader.sh
+++ downloader.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
+
 git_pull() {
-    rm -rf .git/HEAD.lock .git/ORIG_HEAD* .git/refs/heads
-    LOCAL=$(git rev-parse origin/main)
+    rm -rf .git/HEAD.lock .git/ORIG_HEAD* .git/refs/heads > /dev/null
     git fetch --all > /dev/null
-    REMOTE=$(git rev-parse origin/main)
-    
-    if [ $LOCAL != $REMOTE ]; then
-        echo "Pulling changes..."
-        git reset --hard origin/main
-        git pull origin main
-    # else
-        # echo "Already up to date."
-    fi
+    git reset --hard origin/main > /dev/null
+    git pull origin main > /dev/null
 }
 
 git_pull
@@ -19,6 +12,6 @@ git_pull
 npx nodemon &
 
 while true; do
-    sleep 0
+    sleep 5
     git_pull
 done
