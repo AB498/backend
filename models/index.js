ee39fb14893ab79c183cee5f85c9557d8074f61f
AB498 <abcd49800@gmail.com>
2024-02-11 01:24:04 +0600

    Sun, Feb 11, 2024  1:24:04 AM

--- models/index.js
+++ models/index.js
@@ -1,47 +1,48 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'database.sqlite',
-    logging: false
+  dialect: "sqlite",
+  storage: "database.sqlite",
+  logging: false,
 });
 
 let StringArrayType = (fieldName) => ({
-    type: DataTypes.TEXT,
-    defaultValue: '[]',
-    get() {
-        const vals = this.getDataValue(fieldName);
-        return vals ? JSON.parse(vals) : [];
-    },
-    set(val) {
-        this.setDataValue(fieldName, JSON.stringify(val));
-    },
-});  // for SQLite
+  type: DataTypes.TEXT,
+  defaultValue: "[]",
+  get() {
+    const vals = this.getDataValue(fieldName);
+    return vals ? JSON.parse(vals) : [];
+  },
+  set(val) {
+    this.setDataValue(fieldName, JSON.stringify(val));
+  },
+}); // for SQLite
 
 const User = sequelize.define("User", {
-    name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    password: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    dateOfBirth: DataTypes.DATE,
-    bio: DataTypes.STRING,
-    country: DataTypes.STRING,
-    verificationStatus: { type: DataTypes.STRING, defaultValue: 'pending' },
+  name: DataTypes.STRING,
+  username: DataTypes.STRING,
+  email: DataTypes.STRING,
+  phone: DataTypes.STRING,
+  password: DataTypes.STRING,
+  avatar: DataTypes.STRING,
+  dateOfBirth: DataTypes.DATE,
+  bio: DataTypes.STRING,
+  country: DataTypes.STRING,
+  verificationStatus: { type: DataTypes.STRING, defaultValue: "pending" },
+  jwts: StringArrayType("jwts"),
 });
 const Product = sequelize.define("Product", {
-    name: DataTypes.STRING,
-    price: DataTypes.NUMBER,
-    description: DataTypes.STRING,
-    category: StringArrayType('category'),
-    images: StringArrayType('images'),
+  name: DataTypes.STRING,
+  price: DataTypes.NUMBER,
+  description: DataTypes.STRING,
+  category: StringArrayType("category"),
+  images: StringArrayType("images"),
 });
 
 async function init() {
-    await sequelize.sync({
-        force: true
-    });
+  await sequelize.sync({
+    force: true,
+  });
 }
 
 module.exports = { init, sequelize, User, Product };
